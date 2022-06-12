3540f29b8a38a1cd4a672bb6cfb8046f9b4e01d2
Thom9521 <Thomas96mc@gmail.com>
2022-06-12 10:17:11 +0200

    Overflow fix with tools

--- src/components/tools/Tools.js
+++ src/components/tools/Tools.js
@@ -7,26 +7,29 @@ const Tools = () => {
     <React.Fragment>
       <Header title="Tools" emoji="🔨" />
       <div className="container tools">
-        <section>
-          <h1>Frontend</h1>
-          <div className="badges">
-            <span className="badge html5">HTML5</span>
-            <span className="badge css3">CSS3</span>
-            <span className="badge javascript">JavaScript</span>
-            <span className="badge react">React</span>
-            <span className="badge reactNative">React Native</span>
-          </div>
-        </section>
-        <section>
-          <h1>Backend</h1>
-          <div className="badges">
-            <span className="badge php">PHP</span>
-            <span className="badge nodejs">Nodejs</span>
-            <span className="badge mysql">MySQL</span>
-            <span className="badge mongodb">MongoDB</span>
-            <span className="badge api">RESTful API</span>
-          </div>
-        </section>
+        <h3>These are tools that I have worked the most with</h3>
+        <div className="toolsSections">
+          <section>
+            <h1>Frontend</h1>
+            <div className="badges">
+              <span className="badge html5">HTML5</span>
+              <span className="badge css3">CSS3</span>
+              <span className="badge javascript">JavaScript</span>
+              <span className="badge react">React</span>
+              <span className="badge reactNative">React Native</span>
+            </div>
+          </section>
+          <section>
+            <h1>Backend</h1>
+            <div className="badges">
+              <span className="badge php">PHP</span>
+              <span className="badge nodejs">Nodejs</span>
+              <span className="badge mysql">MySQL</span>
+              <span className="badge mongodb">MongoDB</span>
+              <span className="badge api">RESTful API</span>
+            </div>
+          </section>
+        </div>
       </div>
     </React.Fragment>
   );
